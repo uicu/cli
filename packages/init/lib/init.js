36571f4f521ae3b4559599b50d88edd5f1775870
ooyang <ooyang@tencent.com>
2022-04-06 20:43:13 +0800

    add init function

--- packages/init/lib/init.js
+++ packages/init/lib/init.js
@@ -1,8 +1,44 @@
 'use strict';
 
-module.exports = init;
 
-function init() {
-    console.log('我是init');
-    // TODO
+async function init(options) {
+    console.log(options);
+    try {
+      // 设置 targetPath
+      let targetPath = process.cwd();
+      console.log(targetPath);
+      if (!options.targetPath) {
+        options.targetPath = targetPath;
+      }
+      log.verbose('init', options);
+      // 完成项目初始化的准备和校验工作
+    //   const result = await prepare(options);
+    //   if (!result) {
+    //     log.info('创建项目终止');
+    //     return;
+    //   }
+    //   // 获取项目模板列表
+    //   const { templateList, project } = result;
+    //   // 缓存项目模板文件
+    //   const template = await downloadTemplate(templateList, options);
+    //   log.verbose('template', template);
+    //   if (template.type === TEMPLATE_TYPE_NORMAL) {
+    //     // 安装项目模板
+    //     await installTemplate(template, project, options);
+    //   } else if (template.type === TEMPLATE_TYPE_CUSTOM) {
+    //     await installCustomTemplate(template, project, options);
+    //   } else {
+    //     throw new Error('未知的模板类型！');
+    //   }
+    } catch (e) {
+      if (options.debug) {
+        log.error('Error:', e.stack);
+      } else {
+        log.error('Error:', e.message);
+      }
+    } finally {
+      process.exit(0);
+    }
 }
+
+module.exports = init;
