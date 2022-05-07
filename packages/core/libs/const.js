6abc552132fb3fb7f24cd14a601aee636b697672
ooyang <ooyang@tencent.com>
2022-05-07 19:35:22 +0800

    change NPM_NAME

--- packages/core/libs/const.js
+++ packages/core/libs/const.js
@@ -1,6 +1,6 @@
 module.exports = {
   LOWEST_NODE_VERSION: '14.0.0',
   DEFAULT_CLI_HOME: '.uicu-cli',
-  NPM_NAME: '@uicu/cli',
+  NPM_NAME: '@uicu/cli-core',
   DEPENDENCIES_PATH: 'dependencies',
 };
